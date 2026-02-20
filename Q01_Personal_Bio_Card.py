f0cf95ba032446cb68c0a70ad2b11d350ae0a953
Varsha V Nayak <varshavnayak2003@gmail.com>
2026-02-20 21:37:55 +0530

    Added Python file for Q1

---
+++ Q01_Personal_Bio_Card.py
@@ -0,0 +1,12 @@
+# Question 1: Personal Bio Card
+# Write a program that displays your information in a formatted card
+
+print(" _________________________________________________")
+print("|                STUDENT BIO CARD                 |")
+print("|_________________________________________________|")
+print("| Name    : Varsha V Nayak                        |")
+print("| Age     : 22                                    |")
+print("| Course  : Generative AI                         |")
+print("| College : Bangalore Institute of Technology     |")
+print("| Email   : varshavnayak2003@gmail.com            |")
+print("|_________________________________________________|")
