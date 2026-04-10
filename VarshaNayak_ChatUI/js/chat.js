3651d1e05a20874892a2af458a91059efd31a182
Varsha V Nayak <varshavnayak2003@gmail.com>
2026-04-10 23:01:55 +0530

    Added javascript code for chat functionality

--- VarshaNayak_ChatUI/js/chat.js
+++ VarshaNayak_ChatUI/js/chat.js
@@ -3,3 +3,18 @@ const sendBtn = document.querySelector(".send-btn");
 const messages = document.querySelector(".messages");
 const typing = document.querySelector(".typing");
 const welcome = document.querySelector(".welcome");
+
+
+function addMessage(text, sender) {
+  const msg = document.createElement("div");
+  msg.classList.add("message", sender);
+  msg.innerText = text;
+
+  messages.appendChild(msg);
+  scrollToBottom();
+}
+
+
+function scrollToBottom() {
+  messages.scrollTop = messages.scrollHeight;
+}
